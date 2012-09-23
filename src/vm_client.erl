05719ec2a23b800a11b1a75afd1f7c5497e381f5
Rubber Cthulhu <alevandal@gmail.com>
2012-09-23 19:15:08 +0800

    working...

--- src/vm_client.erl
+++ src/vm_client.erl
@@ -12,9 +12,9 @@
 	 handle_call/3, handle_cast/2, handle_info/2,
 	terminate/2, code_change/3]).
 
--export([connect/2, disconnect/1, send/2]).
+-export([connect/2, disconnect/1, send/2, buffer/1]).
 
--record(state, {sock, conn_state}).
+-record(state, {sock, conn_state, buffer}).
 
 -define(KEY, <<16#01, 16#ae, 16#32, 16#ff, 16#d9, 16#13, 16#41, 16#fb>>).
 -define(IVEC, <<16#ab, 16#00, 16#33, 16#49, 16#91, 16#88, 16#ab, 16#cd>>).
@@ -37,7 +37,7 @@ init([]) ->
 handle_call({connect, Host, Port}, _From, State) ->
     case gen_tcp:connect(Host, Port, [binary, {packet, 0}, {active, true}]) of
 	{ok, Sock} ->
-	    NewState = #state{sock = Sock, conn_state = connected},
+	    NewState = #state{sock = Sock, conn_state = connected, buffer = <<>>},
 	    {reply, ok, NewState};
 	Error ->
 	    {reply, Error, State}
@@ -51,6 +51,8 @@ handle_call({send, Payload}, _From, #state{sock = Sock, conn_state = ConnState} 
   when ConnState == connected ->
     Result = gen_tcp:send(Sock, Payload),
     {reply, Result, State};
+handle_call({buffer}, _From, #state{buffer = Buffer} = State) ->
+    {reply, Buffer, State};
 handle_call(_Msg, _From, State) ->
     {reply, {error, unknown_message}, State}.
 %handle_call(Msg, _From, State) ->
@@ -59,14 +61,24 @@ handle_call(_Msg, _From, State) ->
 handle_cast(stop, State) ->
     {stop, normal, State}.
 
-handle_info({tcp, _Sock, RawData}, State) ->
+handle_info({tcp, _Sock, RawData}, #state{buffer = Buffer} = State) ->
     Len = byte_size(RawData),
     Hex = bin_to_hex:bin_to_hex(RawData),
     error_logger:info_msg("Data received: ~w: ~s~n", [Len, Hex]),
-    {noreply, State};
+    case process(<<Buffer/bytes, RawData/bytes>>, fun on_msg_received/2, []) of
+	{ok, Rest, _} ->
+	    ok;
+	{error, Reason, Rest, _} ->
+	    error_logger:error_msg("Process data error: ~p~n", Reason)
+    end,
+    {noreply, State#state{buffer = Rest}};
 handle_info({tcp_closed, _Sock}, State) ->
     error_logger:info_msg("Connection closed~n"),
     {noreply, State}.
+
+on_msg_received(Msg, _) ->
+    error_logger:info_msg("Message received: ~p~n", [Msg]),
+    [].
 
 terminate(_Reason, #state{sock = Sock, conn_state = ConnState} = _State) ->
     case ConnState of
@@ -79,6 +91,19 @@ terminate(_Reason, #state{sock = Sock, conn_state = ConnState} = _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+process(<<>>, _, UserData) ->
+    {ok, <<>>, UserData};
+process(Data, Callback, UserData) ->
+    case vm_msg:decode(?KEY, ?IVEC, Data) of
+	{ok, Msg, Rest} ->
+	    NewUserData = Callback(Msg, UserData),
+	    process(Rest, Callback, NewUserData);
+	{error, data_lack} ->
+	    {ok, Data, UserData};
+	{error, Reason} ->
+	    {error, Reason, Data, UserData}
+    end.
 
 connect(Host, Port) ->
     {ok, Pid} = vm_client_sup:start_child(),
@@ -102,6 +127,9 @@ send(ConnRef, Msg) when is_list(Msg) ->
 send(ConnRef, Msg) when is_binary(Msg) ->
     Data = vm_msg:encode(?KEY, ?IVEC, Msg),
     call(ConnRef, {send, Data}).
+
+buffer(ConnRef) ->
+    call(ConnRef, {buffer}).
 
 call(Pid, Msg) ->
     gen_server:call(Pid, Msg, infinity).
--- src/vm_msg.erl
+++ src/vm_msg.erl
@@ -1,7 +1,9 @@
 
 -module(vm_msg).
 
--export([encode/3]).
+-export([encode/3, decode/3]).
+
+-define(MAX_SIZE, 1024).
 
 encode(Key, IVec, Msg) ->
     Data = padding:pad(Msg),
@@ -9,11 +11,21 @@ encode(Key, IVec, Msg) ->
     Len = byte_size(DesData),
     <<Len:32, DesData/bytes>>.
 
-%decode_len(<<Len:32, Payload/bytes>>) ->
-%    {ok, Len};
-%decode_len(_) ->
-%    {error, size_lack}.
-
+decode(_, _, <<Len:32, _/bytes>>) when Len > ?MAX_SIZE ->
+    {error, max_size};
+decode(Key, IVec, <<Len:32, EncMsg:Len/bytes, Rest/bytes>>) ->
+    try
+	DecMsg = crypto:des_cbc_decrypt(Key, IVec, EncMsg),
+	Msg = padding:unpad(DecMsg),
+	{ok, Msg, Rest}
+    catch
+	throw:Reason ->
+	    {error, Reason};
+	error:Reason ->
+	    {error, Reason}
+    end;
+decode(_, _, _) ->
+    {error, data_lack}.
 
 
 
